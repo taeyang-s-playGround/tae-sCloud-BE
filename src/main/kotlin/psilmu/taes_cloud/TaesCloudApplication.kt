22e2eafb91ce7c77fdd115b8b7138a7f03d8e508
tae.y <0211ilyoil@gmail.com>
2025-04-14 15:03:56 +0900

    setting :: project setting

---
+++ src/main/kotlin/psilmu/taes_cloud/TaesCloudApplication.kt
@@ -0,0 +1,11 @@
+package psilmu.taes_cloud
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TaesCloudApplication
+
+fun main(args: Array<String>) {
+	runApplication<TaesCloudApplication>(*args)
+}
---
+++ src/test/kotlin/psilmu/taes_cloud/TaesCloudApplicationTests.kt
@@ -0,0 +1,13 @@
+package psilmu.taes_cloud
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TaesCloudApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
