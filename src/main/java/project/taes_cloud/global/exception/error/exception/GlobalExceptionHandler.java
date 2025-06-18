ce5eae18844760aad6e1e8b1d6c7df6c1459ae61
tae.y <0211ilyoil@gmail.com>
2025-06-18 09:28:03 +0900

    fix :: 잘못 된 import

--- src/main/java/project/taes_cloud/global/exception/error/exception/GlobalExceptionHandler.java
+++ src/main/java/project/taes_cloud/global/exception/error/exception/GlobalExceptionHandler.java
@@ -1,15 +1,13 @@
-package project.software.global.security.config.error;
+package project.taes_cloud.global.exception.error.exception;
 
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import project.software.global.security.config.error.exception.CustomException;
-import project.software.global.security.config.error.exception.ErrorCode;
-import project.software.global.security.config.error.exception.ErrorResponse;
 
 import java.util.HashMap;
 import java.util.Map;
