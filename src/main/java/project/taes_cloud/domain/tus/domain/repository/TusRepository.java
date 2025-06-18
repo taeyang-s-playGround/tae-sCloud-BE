2b4cc41906b577f336780534708681ad6b998267
tae.y <0211ilyoil@gmail.com>
2025-06-18 09:19:02 +0900

    fix :: tus 임시 제거

--- src/main/java/project/taes_cloud/domain/tus/domain/repository/TusRepository.java
+++
@@ -1,8 +0,0 @@
-package project.taes_cloud.domain.tus.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import project.taes_cloud.domain.tus.domain.Tus;
-
-public interface TusRepository extends JpaRepository<Tus, Long> {
-
-}
