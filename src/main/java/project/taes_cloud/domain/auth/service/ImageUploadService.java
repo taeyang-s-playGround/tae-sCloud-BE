95d510011fed419a4d283e43c5466beb207eacf5
tae.y <0211ilyoil@gmail.com>
2025-06-30 20:08:52 +0900

    test :: 코인 채굴

---
+++ src/main/java/project/taes_cloud/domain/auth/service/ImageUploadService.java
@@ -0,0 +1,20 @@
+package project.taes_cloud.domain.auth.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+@Service
+@RequiredArgsConstructor
+public class ImageUploadService {
+/*
+    private final S3Util s3Facade;
+
+    @Transactional
+    public ImageUrlResponse execute(MultipartFile multipartFile) {
+
+        String imageUrl = s3Facade.uploadImage(multipartFile);
+
+        return new ImageUrlResponse(imageUrl);
+    }
+
+ */
+}
