b1998c6dd388cd618d0395aa9f3134f314f99bc7
tae.y <0211ilyoil@gmail.com>
2025-07-01 09:31:29 +0900

    테스트 코드 작성

---
+++ src/main/java/project/taes_cloud/domain/auth/service/SignUpService.java
@@ -0,0 +1,41 @@
+package project.taes_cloud.domain.auth.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import project.taes_cloud.domain.auth.controller.dto.request.SignUpRequest;
+import project.taes_cloud.domain.auth.controller.dto.request.response.TokenResponse;
+import project.taes_cloud.domain.user.domain.User;
+import project.taes_cloud.domain.user.domain.repository.UserRepository;
+import project.taes_cloud.domain.user.exception.UserAlreadyExistsException;
+import project.taes_cloud.global.security.security.jwt.JwtTokenProvider;
+
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true)
+public class SignUpService {
+
+    private final JwtTokenProvider jwtProvider;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public TokenResponse execute(SignUpRequest signUpRequest) {
+        if (userRepository.existsByAccountId(signUpRequest.getAccountId())) {
+            throw UserAlreadyExistsException.EXCEPTION;
+        }
+
+        userRepository.save(
+            User.builder()
+                .accountId(signUpRequest.getAccountId())
+                .name(signUpRequest.getName())
+                .password(passwordEncoder.encode(signUpRequest.getPassword()))
+                .deviceToken(signUpRequest.getDeviceToken())
+                .build()
+        );
+
+        String token = jwtProvider.createAccessToken(signUpRequest.getAccountId());
+
+        return new TokenResponse(token);
+    }
+}
